cea18dd90ec0986dcce769cf215790c6c9c6af5c
Samriddhi Jain <2025sl93005@wilp.bits-pilani.ac.in>
2026-04-02 10:53:07 +0530

    Added missing calculator functions

--- calculator.py
+++ calculator.py
@@ -1,13 +1,11 @@
-def add(a, b):
+def add(a,b):
     return a + b
 
-def multiply(a, b):
-    return a * b
-
-def subtract(a, b):
+def sub(a,b):
     return a - b
 
-def divide(a, b):
-    if b == 0:
-        return None
+def mul(a,b):
+    return a * b
+
+def div(a,b):
     return a / b
